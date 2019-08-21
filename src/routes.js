1187f1fd23c56eac86ad777755dda20e46b9ba71
Felipe Bergamin <felipebergamin6@gmail.com>
2019-08-21 20:43:09 -0300

    loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'hello world' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Felipe Ricieri Bergamin',
+    email: 'felipebergamin6@gmail.com',
+    password_hash: '434823984928492839482',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
